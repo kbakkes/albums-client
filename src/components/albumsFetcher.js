39f0f7c1488e247d4ab6baf9ef1c24a149bda973
kbakkes <karim.bakkes@gmail.com>
2018-12-28 19:49:55 +0100

    First commit

---
+++ src/components/albumsFetcher.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class AlbumsFether extends Component {
+    render() {
+        return (
+            <h1>test</h1>
+        );
+    }
+}
+
+export default AlbumsFether;
+
